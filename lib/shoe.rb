150f9adc4c95bc9b10e135a6f35f42c84c841c91
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-04 13:07:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,14 @@
 # Make your shoe class here!
 class Shoe
+  #my getter and my accessor, which has get and set functionality
   attr_reader :brand
   attr_accessor :color, :size, :material, :condition
+  
   def initialize(brand)
     @brand = brand
+  end
+  
+  def cobble
+    
   end
 end
